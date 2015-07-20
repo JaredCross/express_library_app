13510350795fcb84885c6f3b5e61363c4d138d3b
Jared Cross <cross.jared@gmail.com>
2015-07-20 15:48:39 -0600

    Restful routing in place

---
+++ routes/library.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/library', function (req, res, next) {
+
+});
+
+router.post('/library', function (req, res, next) {
+
+});
+
+router.get('/library/new', function (req, res, next) {
+
+});
+
+router.get('/library/:id', function (req, res, next) {
+
+});
+
+router.get('/library/:id/edit', function (req, res, next) {
+
+});
+
+router.post('/library/:id/update', function (req, res, next) {
+
+});
+
+router. post('/library/:id/delete', function (req, res, next) {
+
+});
+
+
+
+
+module.exports = router;
